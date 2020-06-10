29bb2b12d24eda23e147a02ff1e170ee9bd3580b
michael anyi <mikeyeucresta@gmail.com>
2020-06-10 01:00:22 +0100

    renamed build directory

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -19,7 +19,7 @@ module.exports = function (config) {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage'),
+      dir: require('path').join(__dirname, './coverage/kobolet'),
       reports: ['html', 'lcovonly', 'text-summary', 'cobertura'],
       fixWebpackSourcePaths: true,
     },
